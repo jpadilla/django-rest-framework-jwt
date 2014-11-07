69659b9646b373d42267c576e5f346c75ccc7310
Andrea Stagi <stagi.andrea@gmail.com>
2014-11-07 10:45:02 +0100

    Invalid payload only if user_id is None

--- rest_framework_jwt/authentication.py
+++ rest_framework_jwt/authentication.py
@@ -70,7 +70,7 @@ class JSONWebTokenAuthentication(BaseAuthentication):
         try:
             user_id = jwt_get_user_id_from_payload(payload)
 
-            if user_id:
+            if user_id is not None:
                 user = User.objects.get(pk=user_id, is_active=True)
             else:
                 msg = 'Invalid payload'
