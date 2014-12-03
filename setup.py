5c53695e6e213e36a9ade43ed08c946554cd3143
José Padilla <jpadilla@webapplicate.com>
2014-12-03 08:20:33 -0400

    Update classifiers

--- setup.py
+++ setup.py
@@ -98,7 +98,12 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
