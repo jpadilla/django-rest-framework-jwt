3caba414521f9d1afa60679b3e41f16ce831589d
Miguel Gonzalez <migonzalvar@gmail.com>
2015-01-14 16:44:50 +0100

    Add final period to expected error strings

--- tests/test_views.py
+++ tests/test_views.py
@@ -265,7 +265,7 @@ class RefreshJSONWebTokenTests(BaseTestCase):
                                format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['non_field_errors'][0],
-                         'Refresh has expired')
+                         'Refresh has expired.')
 
     def tearDown(self):
         # Restore original settings
