2b155821c33f8c38c09901b479a137352947f63c
Jwpe <jwpevans@gmail.com>
2015-02-05 15:07:32 -0500

    Abstracted out logic from the refresh serializer to make a verification serializer. Added a verification view. Added verification tests.

--- rest_framework_jwt/views.py
+++ rest_framework_jwt/views.py
@@ -6,7 +6,10 @@ from rest_framework.response import Response
 
 from rest_framework_jwt.settings import api_settings
 
-from .serializers import JSONWebTokenSerializer, RefreshJSONWebTokenSerializer
+from .serializers import (
+    JSONWebTokenSerializer, RefreshJSONWebTokenSerializer,
+    VerifyJSONWebTokenSerializer
+)
 
 jwt_response_payload_handler = api_settings.JWT_RESPONSE_PAYLOAD_HANDLER
 
@@ -37,20 +40,17 @@ class ObtainJSONWebToken(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class RefreshJSONWebToken(APIView):
+class VerifyJSONWebToken(APIView):
     """
-    API View that returns a refreshed token (with new expiration) based on
-    existing token
-
-    If 'orig_iat' field (original issued-at-time) is found, will first check
-    if it's within expiration window, then copy it to the new token
+    API View that checks the veracity of a token, returning the token if it
+    is valid.
     """
     throttle_classes = ()
     permission_classes = ()
     authentication_classes = ()
     parser_classes = (parsers.FormParser, parsers.JSONParser,)
     renderer_classes = (renderers.JSONRenderer,)
-    serializer_class = RefreshJSONWebTokenSerializer
+    serializer_class = VerifyJSONWebTokenSerializer
 
     def post(self, request):
         serializer = self.serializer_class(data=request.DATA)
@@ -65,5 +65,17 @@ class RefreshJSONWebToken(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class RefreshJSONWebToken(VerifyJSONWebToken):
+    """
+    API View that returns a refreshed token (with new expiration) based on
+    existing token
+
+    If 'orig_iat' field (original issued-at-time) is found, will first check
+    if it's within expiration window, then copy it to the new token
+    """
+    serializer_class = RefreshJSONWebTokenSerializer
+
+
 obtain_jwt_token = ObtainJSONWebToken.as_view()
 refresh_jwt_token = RefreshJSONWebToken.as_view()
+verify_jwt_token = VerifyJSONWebToken.as_view()
