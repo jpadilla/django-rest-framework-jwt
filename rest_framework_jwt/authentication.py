8b237bc516f1cf6cb2662fea5513c86fd17b336d
Alex Rothberg <agrothberg@gmail.com>
2015-02-18 16:48:22 -0500

    Allow subclassing JSONWebTokenAuthentication

--- rest_framework_jwt/authentication.py
+++ rest_framework_jwt/authentication.py
@@ -13,10 +13,56 @@ jwt_decode_handler = api_settings.JWT_DECODE_HANDLER
 jwt_get_user_id_from_payload = api_settings.JWT_PAYLOAD_GET_USER_ID_HANDLER
 
 
-class JSONWebTokenAuthentication(BaseAuthentication):
+class JSONWebTokenAuthenticationBase(BaseAuthentication):
     """
     Token based authentication using the JSON Web Token standard.
+    """
 
+    def authenticate(self, request):
+        """
+        Returns a two-tuple of `User` and token if a valid signature has been
+        supplied using JWT-based authentication.  Otherwise returns `None`.
+        """
+        jwt_value = self.get_jwt_value(request)
+        if jwt_value is None:
+            return None
+
+        try:
+            payload = jwt_decode_handler(jwt_value)
+        except jwt.ExpiredSignature:
+            msg = _('Signature has expired.')
+            raise exceptions.AuthenticationFailed(msg)
+        except jwt.DecodeError:
+            msg = _('Error decoding signature.')
+            raise exceptions.AuthenticationFailed(msg)
+
+        user = self.authenticate_credentials(payload)
+
+        return (user, jwt_value)
+
+    def authenticate_credentials(self, payload):
+        """
+        Returns an active user that matches the payload's user id and email.
+        """
+        User = utils.get_user_model()
+
+        user_id = jwt_get_user_id_from_payload(payload)
+
+        if user_id is not None:
+            try:
+                user = User.objects.get(pk=user_id, is_active=True)
+            except User.DoesNotExist:
+                msg = _('Invalid signature.')
+                raise exceptions.AuthenticationFailed(msg)
+        else:
+            msg = _('Invalid payload.')
+            raise exceptions.AuthenticationFailed(msg)
+
+        return user
+
+
+class JSONWebTokenAuthentication(JSONWebTokenAuthenticationBase):
+    """
     Clients should authenticate by passing the token key in the "Authorization"
     HTTP header, prepended with the string specified in the setting
     `JWT_AUTH_HEADER_PREFIX`. For example:
@@ -25,11 +71,7 @@ class JSONWebTokenAuthentication(BaseAuthentication):
     """
     www_authenticate_realm = 'api'
 
-    def authenticate(self, request):
-        """
-        Returns a two-tuple of `User` and token if a valid signature has been
-        supplied using JWT-based authentication.  Otherwise returns `None`.
-        """
+    def get_jwt_value(self, request):
         auth = get_authorization_header(request).split()
         auth_header_prefix = api_settings.JWT_AUTH_HEADER_PREFIX.lower()
 
@@ -44,38 +86,7 @@ class JSONWebTokenAuthentication(BaseAuthentication):
                     'should not contain spaces.')
             raise exceptions.AuthenticationFailed(msg)
 
-        try:
-            payload = jwt_decode_handler(auth[1])
-        except jwt.ExpiredSignature:
-            msg = _('Signature has expired.')
-            raise exceptions.AuthenticationFailed(msg)
-        except jwt.DecodeError:
-            msg = _('Error decoding signature.')
-            raise exceptions.AuthenticationFailed(msg)
-
-        user = self.authenticate_credentials(payload)
-
-        return (user, auth[1])
-
-    def authenticate_credentials(self, payload):
-        """
-        Returns an active user that matches the payload's user id and email.
-        """
-        User = utils.get_user_model()
-
-        try:
-            user_id = jwt_get_user_id_from_payload(payload)
-
-            if user_id is not None:
-                user = User.objects.get(pk=user_id, is_active=True)
-            else:
-                msg = _('Invalid payload.')
-                raise exceptions.AuthenticationFailed(msg)
-        except User.DoesNotExist:
-            msg = _('Invalid signature.')
-            raise exceptions.AuthenticationFailed(msg)
-
-        return user
+        return auth[1]
 
     def authenticate_header(self, request):
         """
