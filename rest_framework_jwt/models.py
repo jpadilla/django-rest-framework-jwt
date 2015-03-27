391928fb873a56f202b696ddc422dce7254a9e67
Eric Honkanen <eric@epicowl.com>
2015-03-27 16:24:09 -0700

    Moved uuid import to compat

--- rest_framework_jwt/models.py
+++ rest_framework_jwt/models.py
@@ -1,17 +1,10 @@
 from django.db import models
 
-# Django 1.8 includes UUIDField
-if hasattr(models, 'UUIDField'):
-    jti_field = models.UUIDField(editable=False, unique=True)
-else:
-    try:
-        from uuidfield import UUIDField
-        jti_field = UUIDField(auto=False, unique=True)
-    except ImportError:
-        jti_field = models.CharField(max_length=64,
-                                     editable=False, unique=True)
+from .compat import get_uuid_field
+
+UUIDField = get_uuid_field()
 
 
 class JWTBlackListToken(models.Model):
-    jti = jti_field
+    jti = UUIDField()
     timestamp = models.DateTimeField(auto_now_add=True)
