2305fbc0fec57cce3ce3b978385cdbf3eb5502aa
Nicolas Delaby <nicolas.delaby@lock8.me>
2015-05-18 09:23:02 +0200

    Implement .get_serializer() accessor
    It allows user to overrides get_serializer_context() or get_serializer_class()
    when subclassing the view.
    The implementation is taken `from rest_framework/generics.py`

--- rest_framework_jwt/views.py
+++ rest_framework_jwt/views.py
@@ -24,9 +24,40 @@ class JSONWebTokenAPIView(APIView):
     parser_classes = (parsers.FormParser, parsers.JSONParser,)
     renderer_classes = (renderers.JSONRenderer,)
 
+    def get_serializer_context(self):
+        """
+        Extra context provided to the serializer class.
+        """
+        return {
+            'request': self.request,
+            'view': self,
+        }
+
+    def get_serializer_class(self):
+        """
+        Return the class to use for the serializer.
+        Defaults to using `self.serializer_class`.
+        You may want to override this if you need to provide different
+        serializations depending on the incoming request.
+        (Eg. admins get full serialization, others get basic serialization)
+        """
+        assert self.serializer_class is not None, (
+            "'%s' should either include a `serializer_class` attribute, "
+            "or override the `get_serializer_class()` method."
+            % self.__class__.__name__)
+        return self.serializer_class
+
+    def get_serializer(self, *args, **kwargs):
+        """
+        Return the serializer instance that should be used for validating and
+        deserializing input, and for serializing output.
+        """
+        serializer_class = self.get_serializer_class()
+        kwargs['context'] = self.get_serializer_context()
+        return serializer_class(*args, **kwargs)
+
     def post(self, request):
-        serializer = self.serializer_class(data=request.DATA,
-                                           context={'request': request})
+        serializer = self.get_serializer(data=request.DATA)
 
         if serializer.is_valid():
             user = serializer.object.get('user') or request.user
