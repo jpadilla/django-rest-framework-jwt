500723af21a84f95fb32b6960750ff4f1a55c27b
Matt Layman <matthewlayman@gmail.com>
2016-03-30 20:42:18 -0400

    Remove DRF 2.x compatibility functions and conditional definitions.

--- rest_framework_jwt/compat.py
+++ rest_framework_jwt/compat.py
@@ -1,36 +1,22 @@
 from django.contrib.auth import get_user_model
-from distutils.version import StrictVersion
 
-import rest_framework
 from rest_framework import serializers
-from django.forms import widgets
 
 
-DRF_VERSION_INFO = StrictVersion(rest_framework.VERSION).version
-DRF2 = DRF_VERSION_INFO[0] == 2
-DRF3 = DRF_VERSION_INFO[0] == 3
+class Serializer(serializers.Serializer):
+    @property
+    def object(self):
+        return self.validated_data
 
 
-if DRF2:
-    class Serializer(serializers.Serializer):
-        pass
+class PasswordField(serializers.CharField):
 
-    class PasswordField(serializers.CharField):
-        widget = widgets.PasswordInput
-else:
-    class Serializer(serializers.Serializer):
-        @property
-        def object(self):
-            return self.validated_data
-
-    class PasswordField(serializers.CharField):
-
-        def __init__(self, *args, **kwargs):
-            if 'style' not in kwargs:
-                kwargs['style'] = {'input_type': 'password'}
-            else:
-                kwargs['style']['input_type'] = 'password'
-            super(PasswordField, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        if 'style' not in kwargs:
+            kwargs['style'] = {'input_type': 'password'}
+        else:
+            kwargs['style']['input_type'] = 'password'
+        super(PasswordField, self).__init__(*args, **kwargs)
 
 
 def get_username_field():
@@ -49,11 +35,3 @@ def get_username(user):
         username = user.username
 
     return username
-
-
-def get_request_data(request):
-    if DRF2:
-        data = request.DATA
-    else:
-        data = request.data
-    return data
