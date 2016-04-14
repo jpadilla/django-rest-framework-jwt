a914766c4b061acaa2706b0147ad38ace33bc7c0
Shane Grey <shanemgrey@users.noreply.github.com>
2016-04-14 17:52:03 -0500

    Update utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -13,7 +13,7 @@ def jwt_response_payload_handler(token, user=None, request=None):
         return {
             'token': token,
             'user': UserSerializer(user,context={'request': request},).data
-        }   
+        }
 
     """
     return {
