4c8e4d7b296e51eeadf5c847fd86c07890f45b35
Angel Velasquez <avelasquez@sangoma.com>
2016-08-09 19:30:55 -0300

    Add try/catch block when patterns not found (Django 1.10)

--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,8 @@
 """
 Blank URLConf just to keep the test suite happy
 """
-from django.conf.urls import patterns
-
-urlpatterns = patterns('')
+try:
+    from django.conf.urls import patterns
+    urlpatterns = patterns('')
+except ImportError:
+    urlpatterns = []
