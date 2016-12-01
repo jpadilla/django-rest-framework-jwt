a850afab9c6e05254b86f7a8aa264f9184535603
José Padilla <jpadilla@webapplicate.com>
2016-11-30 22:31:13 -0500

    Bump up version

--- rest_framework_jwt/__init__.py
+++ rest_framework_jwt/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'djangorestframework-jwt'
-__version__ = '1.8.0'
+__version__ = '1.9.0'
 __author__ = 'José Padilla'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014-2016 Blimp LLC'
