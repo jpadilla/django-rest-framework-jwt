6528a2a9302d5310f819297105163c50fed666b5
Paweł Krzyżaniak <pawel.krzyzaniak@gmail.com>
2017-08-30 14:58:14 +0200

    Update __init__.py

--- rest_framework_jwt/__init__.py
+++ rest_framework_jwt/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'djangorestframework-jwt'
-__version__ = '1.11.0'
+__version__ = '1.11.2'
 __author__ = 'José Padilla'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014-2017 Blimp LLC'
