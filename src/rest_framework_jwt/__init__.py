f0982582a6a5a8a51d2ce2638ff5a5f50585ac06
Matko Jun <Matko.Jun@styria.hr>
2018-12-13 12:28:29 +0100

    Bump version: 1.12.1 → 1.12.2

--- src/rest_framework_jwt/__init__.py
+++ src/rest_framework_jwt/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'drf-jwt'
-__version__ = '1.12.1'
+__version__ = '1.12.2'
 __author__ = 'José Padilla, Styria Digital'
 __license__ = 'MIT'
 __copyright__ = '2014-2017 Blimp LLC, 2018 Styria Digital'
