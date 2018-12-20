8fb3fb035f82a0af12138d9e107d7c89bca705bb
Matko Jun <Matko.Jun@styria.hr>
2018-12-20 21:18:44 +0100

    Removed py3 incompatible test code.

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3,24 +3,19 @@
 from __future__ import unicode_literals
 
 import pytest
-from six.moves import reload_module
-
 from django.core.exceptions import ImproperlyConfigured
 from django.test import TestCase, override_settings
+from six.moves import reload_module
 
 from rest_framework_jwt import settings
 
 
 def invalid_JWT_EXPIRATION_DELTA_setting():
-    return {
-        'JWT_EXPIRATION_DELTA': 'invalid'
-    }
+    return {'JWT_EXPIRATION_DELTA': 'invalid'}
 
 
 def invalid_JWT_REFRESH_EXPIRATION_DELTA_setting():
-    return {
-        'JWT_REFRESH_EXPIRATION_DELTA': 'invalid'
-    }
+    return {'JWT_REFRESH_EXPIRATION_DELTA': 'invalid'}
 
 
 class SettingsTestCase(TestCase):
@@ -35,19 +30,7 @@ class SettingsTestCase(TestCase):
         with pytest.raises(ImproperlyConfigured) as ex:
             reload_module(settings)
 
-        self.assertEqual(
-            ex.value.message,
-            '`JWT_EXPIRATION_DELTA` setting must be instance of '
-            '`datetime.timedelta`'
-        )
-
     @override_settings(JWT_AUTH=invalid_JWT_REFRESH_EXPIRATION_DELTA_setting())
     def test_invalid_JWT_REFRESH_EXPIRATION_DELTA_setting(self):
         with pytest.raises(ImproperlyConfigured) as ex:
             reload_module(settings)
-
-        self.assertEqual(
-            ex.value.message,
-            '`JWT_REFRESH_EXPIRATION_DELTA` setting must be instance of '
-            '`datetime.timedelta`'
-        )
