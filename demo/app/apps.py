a35ddb6727a8ec7834a88657a72078e65615f398
Filip Todic <todic.filip@gmail.com>
2019-12-18 12:04:05 +0100

    Use a demo project for running the test suite
    When all test environments were run locally, the migration files would
    persist. This would result in `NodeNotFoundError` being raised when
    ran against a version of Django that does not contain that migration
    file.
    
    This change introduces a migration file that is connected to Django's
    0008 `auth` migration. Since the change to the custom `User` model is
    trivial, this should not be an issue when tested against future versions
    of Django.

---
+++ demo/app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppConfig(AppConfig):
+    name = "app"
+    label = "app"
---
+++ demo/app/urls.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from rest_framework_jwt import views
+from rest_framework_jwt.compat import url
+
+from .views import test_view
+
+
+urlpatterns = [
+    url(r"^auth/$", views.obtain_jwt_token, name="auth"),
+    url(r"^auth/verify/$", views.verify_jwt_token, name="auth-verify"),
+    url(r"^auth/refresh/$", views.refresh_jwt_token, name="auth-refresh"),
+    url(r"^test-view/$", test_view, name="test-view"),
+]
