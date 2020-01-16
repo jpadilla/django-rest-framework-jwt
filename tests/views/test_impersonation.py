ec62ee41888ab157903f23d12b20653bc758a6ae
Nikolina Krizanec <nikolina.krizanec@t-matix.com>
2020-01-16 14:45:23 +0100

    Test regarding impersonate endpoint

--- tests/views/test_impersonation.py
+++ tests/views/test_impersonation.py
@@ -4,10 +4,11 @@ from __future__ import unicode_literals
 
 from rest_framework import status
 from rest_framework.reverse import reverse
-from rest_framework_jwt.settings import api_settings
 
 
-def test_superuser_can_impersonate(user, super_user, create_authenticated_client):
+def test_superuser_can_impersonate(
+        user, super_user, create_authenticated_client
+):
 
     api_client = create_authenticated_client(super_user)
 
@@ -19,7 +20,9 @@ def test_superuser_can_impersonate(user, super_user, create_authenticated_client
     assert "token" in response.json()
 
 
-def test_staff_user_can_impersonate(user, staff_user, create_authenticated_client):
+def test_staff_user_cannot_impersonate(
+        user, staff_user, create_authenticated_client
+):
 
     api_client = create_authenticated_client(staff_user)
 
@@ -27,8 +30,7 @@ def test_staff_user_can_impersonate(user, staff_user, create_authenticated_clien
     url = reverse("impersonate")
     response = api_client.post(url, data, format="json")
 
-    assert response.status_code == status.HTTP_200_OK
-    assert "token" in response.json()
+    assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
 def test_normal_user_cannot_impersonate(user, create_authenticated_client):
@@ -50,7 +52,9 @@ def test_anonymous_user_cannot_impersonate(api_client):
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
-def test_serializer_not_valid_superuser(super_user, create_authenticated_client):
+def test_serializer_not_valid_superuser(
+    super_user, create_authenticated_client
+):
 
     api_client = create_authenticated_client(super_user)
 
@@ -59,14 +63,3 @@ def test_serializer_not_valid_superuser(super_user, create_authenticated_client)
     response = api_client.post(url, data, format="json")
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
-
-
-def test_user_return_token_and_cookie(monkeypatch, api_client):
-
-    url = reverse("impersonate")
-    response = api_client.post(url, format="json")
-
-    imp_cookie = "jwt-imp"
-    monkeypatch.setattr(api_settings, "JWT_IMPERSONATION_COOKIE", imp_cookie)
-
-    assert imp_cookie in response.cookies
