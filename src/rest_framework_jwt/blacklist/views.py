ab026ff4129ca244e76c96f4864dcc868364c2f5
mzugcic <zuga89@gmail.com>
2020-01-28 15:36:50 +0100

    Add admin cant blacklist superuser functionality & test

--- src/rest_framework_jwt/blacklist/views.py
+++ src/rest_framework_jwt/blacklist/views.py
@@ -25,21 +25,25 @@ class BlacklistView(ModelViewSet):
 
         token = serializer.data.get('token')
         token_payload = JSONWebTokenAuthentication.jwt_decode_token(token)
-        token_username = JSONWebTokenAuthentication.jwt_get_username_from_payload(token_payload)
 
-        if token_username != request.user.username:
-            if not request.user.is_superuser:
+        token_user = check_user(token_payload)
+
+        if token_user.username != request.user.username:
+            if not request.user.is_staff or token_user.is_superuser:
                 return Response(status=status.HTTP_403_FORBIDDEN)
 
         iat = token_payload.get('iat', unix_epoch())
         expires_at_unix_time = iat + api_settings.JWT_EXPIRATION_DELTA.total_seconds()
 
-        blacklisted_token, _ = BlacklistedToken.objects.get_or_create(
+        blacklisted_token, created = BlacklistedToken.objects.get_or_create(
             token=token,
             user=check_user(payload=token_payload),
             expires_at=datetime.utcfromtimestamp(expires_at_unix_time),
-            blacklisted_by=request.user,
         )
+        if created:
+            blacklisted_token.blacklisted_by=request.user
+            blacklisted_token.save()
+
         response = Response(
             JSONWebTokenAuthentication.jwt_create_response_payload(
                 blacklisted_token.token, request.user, request
--- tests/views/test_blacklist.py
+++ tests/views/test_blacklist.py
@@ -21,6 +21,21 @@ def test_user_can_blacklist_own_token(user, create_authenticated_client):
     assert BlacklistedToken.objects.exists()
 
 
+def test_staff_user_can_blacklist_user_token(
+    user, staff_user, create_authenticated_client
+):
+    payload = JSONWebTokenAuthentication.jwt_create_payload(user)
+
+    url = reverse('blacklist-list')
+    data = {'token': JSONWebTokenAuthentication.jwt_encode_payload(payload)}
+
+    api_client_staff_user = create_authenticated_client(staff_user)
+    response = api_client_staff_user.post(url, data)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert BlacklistedToken.objects.count() == 1
+
+
 def test_super_user_can_blacklist_anyones_token(
     user, staff_user, super_user, create_authenticated_client
 ):
@@ -46,7 +61,29 @@ def test_super_user_can_blacklist_anyones_token(
 def test_user_cannot_blacklist_someones_token(
     user, staff_user, super_user, create_authenticated_client
 ):
-    payload = JSONWebTokenAuthentication.jwt_create_payload(user)
+    payload = JSONWebTokenAuthentication.jwt_create_payload(staff_user)
+    url = reverse('blacklist-list')
+    data = {'token': JSONWebTokenAuthentication.jwt_encode_payload(payload)}
+
+    api_client = create_authenticated_client(user)
+    response = api_client.post(url, data)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert not BlacklistedToken.objects.exists()
+
+    payload = JSONWebTokenAuthentication.jwt_create_payload(super_user)
+    data = {'token': JSONWebTokenAuthentication.jwt_encode_payload(payload)}
+
+    response = api_client.post(url, data)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert not BlacklistedToken.objects.exists()
+
+
+def test_admin_cannot_blacklist_superuser(
+    staff_user, super_user, create_authenticated_client
+):
+    payload = JSONWebTokenAuthentication.jwt_create_payload(super_user)
     url = reverse('blacklist-list')
     data = {'token': JSONWebTokenAuthentication.jwt_encode_payload(payload)}
 
@@ -56,13 +93,23 @@ def test_user_cannot_blacklist_someones_token(
     assert response.status_code == status.HTTP_403_FORBIDDEN
     assert not BlacklistedToken.objects.exists()
 
-    payload = JSONWebTokenAuthentication.jwt_create_payload(staff_user)
+
+def test_blacklisted_token_cannot_be_blacklisted_again(
+    user, staff_user, super_user, create_authenticated_client
+):
+    payload = JSONWebTokenAuthentication.jwt_create_payload(user)
 
     url = reverse('blacklist-list')
     data = {'token': JSONWebTokenAuthentication.jwt_encode_payload(payload)}
 
-    api_client = create_authenticated_client(user)
-    response = api_client.post(url, data)
+    api_client_super_user = create_authenticated_client(super_user)
+    response = api_client_super_user.post(url, data)
 
-    assert response.status_code == status.HTTP_403_FORBIDDEN
-    assert not BlacklistedToken.objects.exists()
+    assert response.status_code == status.HTTP_200_OK
+    assert BlacklistedToken.objects.count() == 1
+
+    api_client_staff_user = create_authenticated_client(staff_user)
+    response = api_client_staff_user.post(url, data)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert BlacklistedToken.objects.count() == 1
