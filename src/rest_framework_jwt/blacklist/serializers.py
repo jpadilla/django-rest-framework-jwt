8b5d8f594703379257f787cf7b15a874d31008ef
mzugcic <zuga89@gmail.com>
2020-01-29 15:39:03 +0100

    Move logic from views create to serializators validate method

--- src/rest_framework_jwt/blacklist/serializers.py
+++ src/rest_framework_jwt/blacklist/serializers.py
@@ -2,15 +2,42 @@
 
 from __future__ import unicode_literals
 
+from datetime import datetime
+
+from django.utils.timezone import make_aware
+
 from rest_framework import serializers
 
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework_jwt.blacklist.models import BlacklistedToken
 from rest_framework_jwt.settings import api_settings
-from rest_framework_jwt.utils import check_payload, check_user, unix_epoch
+from rest_framework_jwt.utils import unix_epoch
 
 
-class BlacklistTokenSerializer(serializers.Serializer):
+class BlacklistTokenSerializer(serializers.ModelSerializer):
     """
     Serializer used for blacklisting tokens.
     """
 
-    token = serializers.CharField(required=False)
+    class Meta:
+        model = BlacklistedToken
+        fields = ('user', )
+
+    def validate(self, data):
+        user = data.get('user')
+
+        payload = JSONWebTokenAuthentication.jwt_create_payload(user)
+        token = JSONWebTokenAuthentication.jwt_encode_payload(payload)
+
+        iat = payload.get('iat', unix_epoch())
+        expires_at_unix_time = iat + api_settings.JWT_EXPIRATION_DELTA.total_seconds()
+
+        return {
+            'token':
+                token,
+            'user':
+                user,
+            'expires_at':
+                make_aware(datetime.utcfromtimestamp(expires_at_unix_time)),
+            'blacklisted_by': self.context['request'].user
+        }
--- src/rest_framework_jwt/blacklist/views.py
+++ src/rest_framework_jwt/blacklist/views.py
@@ -1,52 +1,23 @@
-from datetime import datetime
+from django.db import IntegrityError
 
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 from rest_framework.viewsets import ModelViewSet
 
 from rest_framework_jwt.blacklist.models import BlacklistedToken
 
 from ..authentication import JSONWebTokenAuthentication
-from ..settings import api_settings
-from ..utils import check_user, unix_epoch
+from .permissions import CanBlacklist
 from .serializers import BlacklistTokenSerializer
 
 
 class BlacklistView(ModelViewSet):
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (CanBlacklist, )
     authentication_classes = (JSONWebTokenAuthentication, )
     queryset = BlacklistedToken.objects.all().order_by('-blacklisted_at')
     serializer_class = BlacklistTokenSerializer
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        token = serializer.data.get('token')
-        token_payload = JSONWebTokenAuthentication.jwt_decode_token(token)
-
-        token_user = check_user(token_payload)
-
-        if token_user.username != request.user.username:
-            if not request.user.is_staff or token_user.is_superuser:
-                return Response(status=status.HTTP_403_FORBIDDEN)
-
-        iat = token_payload.get('iat', unix_epoch())
-        expires_at_unix_time = iat + api_settings.JWT_EXPIRATION_DELTA.total_seconds()
-
-        blacklisted_token, created = BlacklistedToken.objects.get_or_create(
-            token=token,
-            user=check_user(payload=token_payload),
-            expires_at=datetime.utcfromtimestamp(expires_at_unix_time),
-        )
-        if created:
-            blacklisted_token.blacklisted_by=request.user
-            blacklisted_token.save()
-
-        response = Response(
-            JSONWebTokenAuthentication.jwt_create_response_payload(
-                blacklisted_token.token, request.user, request
-            )
-        )
-        return response
+        try:
+            return super(BlacklistView, self).create(request, *args, **kwargs)
+        except IntegrityError:
+            raise ValidationError('User\'s token has already been blacklisted.')
