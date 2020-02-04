d0965d6aa9ed1fd09ad5997beff2b6478651b700
cparker-zen <54034272+cparker-zen@users.noreply.github.com>
2020-02-04 11:22:43 -0600

    Update compat.py

--- src/rest_framework_jwt/compat.py
+++ src/rest_framework_jwt/compat.py
@@ -7,3 +7,9 @@ try:
     from django.urls import include, url
 except ImportError:
     from django.conf.urls import include, url  # noqa: F401
+
+try:
+    from django.utils.encoding import smart_text
+except ImportError:
+    from django.utils.encoding import smart_str as smart_text
+    
