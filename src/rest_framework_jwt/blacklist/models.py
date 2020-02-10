4428ac6118def48ecb427866a23bb4065de146ed
mzugcic <zuga89@gmail.com>
2020-02-10 17:26:44 +0100

    Refactor BlacklistToken manager & remove related_name from user

--- src/rest_framework_jwt/blacklist/models.py
+++ src/rest_framework_jwt/blacklist/models.py
@@ -1,18 +1,22 @@
-from datetime import datetime
+# -*- coding: utf-8 -*-
 
 from django.conf import settings
 from django.db import models
 
+from rest_framework_jwt.settings import api_settings
+
 
 class BlacklistedTokenManager(models.Manager):
     def delete_stale_tokens(self):
-        return self.filter(expires_at__lt=datetime.utcnow()).delete()
+        return self.filter(
+            expires_at__lt=api_settings.JWT_STALE_BLACKLISTED_TOKEN_EXPIRATION_TIME
+        ).delete()
 
 
 class BlacklistedToken(models.Model):
     token = models.TextField(db_index=True, unique=True)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='owner')
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     expires_at = models.DateTimeField(db_index=True)
     blacklisted_at = models.DateTimeField(auto_now_add=True)
 
