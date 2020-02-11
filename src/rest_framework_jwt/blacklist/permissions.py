04db67b1523f3e19db1a09c22d09329668f19455
mzugcic <zuga89@gmail.com>
2020-02-11 15:57:47 +0100

    Add impersonation cookie to IsNotBlacklisted permission

--- src/rest_framework_jwt/blacklist/permissions.py
+++ src/rest_framework_jwt/blacklist/permissions.py
@@ -9,8 +9,15 @@ class IsNotBlacklisted(BasePermission):
     message = 'You have been blacklisted.'
 
     def has_permission(self, request, view):
-        if api_settings.JWT_AUTH_COOKIE:
-            token = request.COOKIES.get(api_settings.JWT_AUTH_COOKIE)
+        if not request.auth:
+            if api_settings.JWT_IMPERSONATION_COOKIE:
+                imp_user_token = request.COOKIES.get(api_settings.JWT_IMPERSONATION_COOKIE)
+                if imp_user_token:
+                    token = imp_user_token
+
+            if api_settings.JWT_AUTH_COOKIE:
+                token = request.COOKIES.get(api_settings.JWT_AUTH_COOKIE)
+
         else:
             token = force_str(request.auth)
 
